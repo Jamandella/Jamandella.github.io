bac53ecb5294591734b36bd5c2fc04ed81bcd032
Josh <Jo.mandella@gmail.com>
2022-03-15 03:04:27 -0400

    Added API endpoints to ping mysql database and user query in App.js

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
 import json
+import mysql.connector
+
 
 app = Flask(__name__)
 api = Api(app)
-
+CORS(app)
+#protocol//[hosts][ILikeTurtles][turtles]
 
 class Quotes(Resource):
     """First test class"""
@@ -14,12 +18,36 @@ class Quotes(Resource):
         param = request.args.get('param')
         return json.dumps({'success':True, 'param': param}), 200, {'ContentType':'application/json'}
 
-    def get(self):
-        return {
-            "Hello": "world",
-            "query_parap": "{value}"
-            }
+    @app.route('/login/')
+    def login():
+        username = request.args.get('username')
 
+    def get(self):
+
+        cnx = mysql.connector.connect(user='root', password='thecolorblue970710',
+                                      host='127.0.0.1',
+                                      database='ILikeTurtles')
+        mycursor = cnx.cursor()
+
+        mycursor.execute("SELECT * FROM User")
+
+        myresult = mycursor.fetchall()
+        cnx.close()
+
+        return jsonify({
+            "User" : str(myresult),
+            "Hello": "world",
+            #query_parap: {value}
+            })
+ #   def protocol(self):
+ #       return jdbc:mysql:replication//myUser:myPassword@[address=(host=myHost1)(port=1111)(key1=value1)]
+
+    def post():
+        data = request.get_json()     # status code
+        return jsonify({'data': data}), 201
+
+api.add_resource(Quotes, '/')
+#api.add_resource(Square, '/square/<int:num>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false,
+      value: "",
+		};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+/*	componentDidMount() {
+		fetch("http://localhost:5000")
+			.then((res) => res.json())
+			.then((json) => {
+console.log(json);
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}*/
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    fetch(`http://localhost:5000?name=${this.state.value}`)
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          data: json
+        })
+      })
+    event.preventDefault();
+  }
+
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded)
+      return (
+        <div>
+			    <h1> Pleses wait some time.... </h1>
+        </div>
+      );
+
+		return (
+      <div className = "App">
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <h1> Fetch data from an api in react </h1> {
+          [items].map((item) => (
+          <ol key = { item.id } >
+            Username: { item.User },
+            </ol>
+          ))
+        }
+      </div>
+	  );
+  }
 }
 
 export default App;
+
